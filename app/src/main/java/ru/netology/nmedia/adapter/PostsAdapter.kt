0e28b026005de230dd778c2b61e895370a09cb43
IvanKlim1 <vanchester246@gmail.com>
2021-07-19 12:26:55 +0300

    Fix view

--- app/src/main/java/ru/netology/nmedia/adapter/PostsAdapter.kt
+++ app/src/main/java/ru/netology/nmedia/adapter/PostsAdapter.kt
@@ -1,6 +1,7 @@
 package ru.netology.nmedia.adapter
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.PopupMenu
 import androidx.recyclerview.widget.DiffUtil
@@ -14,24 +15,25 @@ import java.text.DecimalFormat
 private val df = DecimalFormat("#.#")
 private val dff = DecimalFormat("#")
 
-interface ExtensionForAdapterFunctions{
-    fun liked(post:Post){}
-    fun reposted(post:Post){}
+interface ExtensionForAdapterFunctions {
+    fun liked(post: Post) {}
+    fun reposted(post: Post) {}
     fun onEdit(post: Post) {}
     fun onRemove(post: Post) {}
 }
-class PostDiffCallback: DiffUtil.ItemCallback<Post>(){
-override fun areItemsTheSame(oldItem:Post,newItem:Post):Boolean{
-    return oldItem.id==newItem.id
-}
+
+class PostDiffCallback : DiffUtil.ItemCallback<Post>() {
+    override fun areItemsTheSame(oldItem: Post, newItem: Post): Boolean {
+        return oldItem.id == newItem.id
+    }
 
     override fun areContentsTheSame(oldItem: Post, newItem: Post): Boolean {
-        return oldItem==newItem
+        return oldItem == newItem
     }
 }
 
 class PostsAdapter(private val callBack: ExtensionForAdapterFunctions) :
-    ListAdapter<Post,PostViewHolder>(PostDiffCallback()) {
+    ListAdapter<Post, PostViewHolder>(PostDiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
         val binding = CardPostBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -53,10 +55,11 @@ class PostViewHolder(
             author.text = post.author
             published.text = post.published
             content.text = post.content
-            like.isChecked=post.likedByMe
-            repost.isChecked=post.repostByMe
+            like.isChecked = post.likedByMe
+            repost.isChecked = post.repostByMe
             menu.setOnClickListener {
-                val popup = PopupMenu(it.context, it).apply {
+                binding.menu.isChecked = true
+                PopupMenu(it.context, it).apply {
                     inflate(R.menu.options_post)
                     setOnMenuItemClickListener { item ->
                         when (item.itemId) {
@@ -71,10 +74,10 @@ class PostViewHolder(
                             else -> false
                         }
                     }
-                }
-                menu.setOnClickListener {
-                    popup.show()
-                }
+                    setOnDismissListener {
+                        binding.menu.isChecked = false
+                    }
+                }.show()
             }
             like.text = countFormat(post.likes)
             repost.text = countFormat(post.reposts)
@@ -89,14 +92,13 @@ class PostViewHolder(
 
     private fun countFormat(count: Int): String =
 
-    when
-    {
-        count < 1_000 -> count.toString()
-        count == 1_000 || count < 1_100 -> dff.format(count / 1000)+"K"
-        count in 1_100..9_999 -> df.format(count / 1000)+"K"
-        count in 10_000..999_999 -> dff.format(count / 1000)+"K"
-        count in 1000000..1099999 -> dff.format(count / 1000000)+"M"
-        count > 1099999 -> df.format(count / 1000000)+"M"
-        else -> count.toString()
-    }
+        when {
+            count < 1_000 -> count.toString()
+            count == 1_000 || count < 1_100 -> dff.format(count / 1000) + "K"
+            count in 1_100..9_999 -> df.format(count / 1000) + "K"
+            count in 10_000..999_999 -> dff.format(count / 1000) + "K"
+            count in 1000000..1099999 -> dff.format(count / 1000000) + "M"
+            count > 1099999 -> df.format(count / 1000000) + "M"
+            else -> count.toString()
+        }
 }
